0de636d55a49c4bffadd4b497d982a3f412fb929
unknown <alu0100894646@ull.edu.es>
2019-04-04 11:06:26 +0100

    Ajax

--- practica_ajax.js
+++ practica_ajax.js
@@ -1,7 +1,20 @@
-
-$(document).ready(function() {
-  $("button").click(function(){
-    $("#test_button").text("Hola Mundo!");
-    console.log("miboton");
-  });
+$(document).ready(function () {
+    $("#mybutton").click(function () {
+        console.log("debug");
+        $.ajax({
+            method: "GET",
+            url: "https://jsonplaceholder.typicode.com/comments?postId=1",
+            headers: { 'Access-Control-Allow-Origin': '*' },
+            crossDomain: true,
+            success: function (data) {
+                alert("success");
+                console.log("success");
+                var data_json = JSON.stringify(data)//,null,2);
+                $("#test_button").html(data_json);
+            },
+            error: function (status) {
+                alert("Exception generated: " + status);
+            },
+        });
+    });
 });
