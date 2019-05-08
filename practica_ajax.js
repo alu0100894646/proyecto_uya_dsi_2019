ab640f6cc34554127098853a5e17b71395c78925
unknown <alu0100894646@ull.edu.es>
2019-05-08 16:16:47 +0100

    fetch

--- practica_ajax.js
+++ practica_ajax.js
@@ -1,7 +1,30 @@
 
-$(document).ready(function() {
-  $("button").click(function(){
-    $("#test_button").text("Hola Mundo!");
-    console.log("miboton");
+/*$(document).ready(function() {
+    $("#mybutton").click(function () {
+        console.log("hola");
+        $.ajax({
+            method: "GET",
+            url: "https://jsonplaceholder.typicode.com/comments?postId=1",
+            data: $("#test_button").load(),
+            success: function () {
+                console.log("funciona");
+            },
+            error: function (jqxhr, status, exception) {
+                console.log("Exception:" + status);
+            },
+            complete: function (xhr, status) {
+                console.log("Petición realizada con exito.");
+            }
+
+        });
+    //$("#test_button").text("Hola Mundo!");
+    //aconsole.log("mibotn");
   });
+});*/
+
+$(document).ready(function () {
+    $("#mybutton").click(function () {
+        console.log("hola");
+    });
+
 });
