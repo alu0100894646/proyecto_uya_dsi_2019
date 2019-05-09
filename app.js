82e8bde1971f9aea1dc61bb6aaff42d7e21192cb
JOSÉ FRANCISCO <alu0100894646@ull.edu.es>
2019-05-09 11:25:56 +0100

    cambios en database y tal

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 function enviarForm(){
       // Initialize Firebase
-
-
       const form = document.getElementById('formulario')
 
 
       function enviarForm(event){
           event.preventDefault();
-
+        };
+       var id = 4;
        var titulo = document.getElementById('titulo');
        var autor = document.getElementById('autor');
        var español = document.getElementById('español');
        var ingles = document.getElementById('ingles');
        var genero = document.getElementById('genero');
-
-
 
        var data = {
          'titulo': titulo.value,
@@ -23,14 +20,27 @@ function enviarForm(){
          'ingles': ingles.value,
          'genero': genero.value
        };
-      };
 
-function saveContactForm(data) {
-    firebase.database().ref('formulary-f1e07').push(data).then(function() {
-alert('dato almacenado correctamente');})
- .catch(function(error){
- console.log('detectado un error', error);
- });
+      var database_ref = firebase.database().ref();
+      var newInput = database_ref.push();
+
+
+      newInput.set({
+        autor: autor,
+        genero:genero,
+        titulo: titulo
+      });
+
+
+
+
+
+      /*function saveContactForm(data) {
+          firebase.database().ref('formulary-f1e07').push(data).then(function() {
+      alert('dato almacenado correctamente');})
+       .catch(function(error){
+       console.log('detectado un error', error);
+       });
+     };*/
+
 };
-
-}
