ed58c0aa8242cde39e12936d82d9dc588460ad18
alu0100893861 <43882478+alu0100893861@users.noreply.github.com>
2019-05-09 10:14:07 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 
       // Initialize Firebase
-      var firebaseConfig = {
-    apiKey: "AIzaSyAFlF9buXw6HuxZGfw9ANjnaQkSP9_dBKI",
-    authDomain: "formulary-f1e07.firebaseapp.com",
-    databaseURL: "https://formulary-f1e07.firebaseio.com",
-    projectId: "formulary-f1e07",
-    storageBucket: "formulary-f1e07.appspot.com",
-    messagingSenderId: "405576541388",
-    appId: "1:405576541388:web:44f4753dbf6c680d"
-  };
-     firebase.initializeApp(firebaseConfig);
+
 
       const form = document.getElementById('formulario')
-      form.addEventListener('submit', enviarForm);
+      
 
       function enviarForm(event){
           event.preventDefault();
@@ -35,7 +26,7 @@
       };
 
 function saveContactForm(data) {
-    firebase.database().ref('formulary-f1e07').set(data).then(function() {
+    firebase.database().ref('formulary-f1e07').push(data).then(function() {
 alert('dato almacenado correctamente');})
  .catch(function(error){
  console.log('detectado un error', error);
