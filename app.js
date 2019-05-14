33b3610bf98229aaf6790ac062d5868716097c63
unknown <alu0100894646@ull.edu.es>
2019-05-14 21:04:16 +0100

    local push final app

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 function enviarForm() {
-    var database = firebase.database();
-    var ref = firebase.database().ref('books/');
-    console.log(database);
+    //Firebase initialization
     
+    var ref = firebase.database().ref('books/');
+
+    //Fetch data from the form
+    var title = document.getElementById('titulo').value;
+    var autor = document.getElementById('autor').value;
+    var values = $('#genero').val();
+
+    var checkEsp = $('#español:checked').val();
+    var checkEng = $('#ingles:checked').val();
+    var idiomas = [];
+
+    if (checkEsp != null)
+        idiomas.push(checkEsp);
+
+    if (checkEng != null)
+        idiomas.push(checkEng);
+
     var max_id;
     ref.once('value').then(function (snapshot) {
         max_id = snapshot.numChildren();
-
+        max_id +=1;
         ref.push({
             id: max_id,
-            Titulo: "Prueba",
-
+            Titulo: title,
+            Autor : autor,
+            Genero: values,
+            Idiomas: idiomas
         });
     });
 };
